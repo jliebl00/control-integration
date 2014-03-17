3002b485a7073f8d22da5d8df58be0e03e95dc55
Patricia <Patricia@Patricia.Home>
2014-03-17 23:27:32 +0100

    carpetas para recursos creadas

--- src/es/unileon/ulebank/Office.java
+++ src/es/unileon/ulebank/Office.java
@@ -10,41 +10,63 @@ import es.unileon.ulebank.handler.IdOffice;
  */
 public class Office {
 
+	/**
+	 * The id of the office
+	 */
 	IdOffice idOffice;
 	// ArrayList<Account> accountList;
+	/**
+	 * The list of employees of this office
+	 */
 	ArrayList<Employee> employeeList;
 
+	/**
+	 * Constructor of the class
+	 */
 	public Office(IdOffice idOffice) {
 		this.idOffice = idOffice;
 	}
 
+	/**
+	 * Returns the id of the office
+	 */
 	public IdOffice getIdOffice() {
 		return idOffice;
 	}
 
+	/**
+	 * Sets the id of the office
+	 */
 	public void setIdOffice(IdOffice idOffice) {
 		this.idOffice = idOffice;
 	}
 
+	/**
+	 * Returns the list of employees of the office
+	 */
 	public ArrayList<Employee> getEmployeeList() {
 		return employeeList;
 	}
 
+	/**
+	 * Sets the list of employees of the office
+	 */
 	public void setEmployeeList(ArrayList<Employee> employeeList) {
 		this.employeeList = employeeList;
 	}
-	
-	public void addEmployee(Employee employee){
+
+	public void addEmployee(Employee employee) {
 		employeeList.add(employee);
 	}
-	public void deleteEmployee(Employee employee){
+
+	public void deleteEmployee(Employee employee) {
 		employeeList.remove(employee);
 	}
-//	public void addAccount(Account account){
-//		employeeList.add(employee);
-//	}
-//	public void deleteAccount(Account account){
-//		employeeList.remove(employee);
-//	}
+	// public void addAccount(Account account){
+	// employeeList.add(employee);
+	// }
+	// public void deleteAccount(Account account){
+	// employeeList.remove(employee);
+	// }
 
 }
