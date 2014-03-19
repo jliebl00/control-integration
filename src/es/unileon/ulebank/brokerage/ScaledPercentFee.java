5d6a03b5e5b6b1ced80863b2a845f7523599cc46
Roobre <roobre@roobre.es>
2014-03-19 16:14:31 +0100

    Implemented ScaledPercentFee.

--- src/es/unileon/ulebank/brokerage/ScaledPercentFee.java
+++ src/es/unileon/ulebank/brokerage/ScaledPercentFee.java
@@ -1,22 +1,56 @@
 package es.unileon.ulebank.brokerage;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ScaledPercentFee implements FeeStrategy {
 
     private final ArrayList<FeeStep> steps;
-    
-    public ScaledPercentFee() {
+    private final double minimum;
+
+    public ScaledPercentFee(double minimum) {
         this.steps = new ArrayList<>();
+        this.minimum = minimum;
     }
-    
+
     public void addStep(FeeStep step) throws CrossedStepException {
+
+        if (this.steps.size() > 0) {
+            Iterator<FeeStep> it = this.steps.iterator();
+            FeeStep fs;
+            
+            do {
+                fs = it.next();
+                if (fs.collides(step)) {
+                    throw new CrossedStepException(fs, step);
+                }
+            } while (it.hasNext());
+        }
+
         this.steps.add(step);
     }
-    
+
+    /**
+     * Returns the fee that should be applied to the given amount.
+     *
+     * @param value
+     * @return The fee that should be applied to the given amount
+     */
     @Override
     public double getFee(double value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean match;
+        FeeStep fs = null;
+
+        if (this.steps.size() > 0) {
+            Iterator<FeeStep> it = this.steps.iterator();
+            do {
+                fs = it.next();
+                match = fs.wraps(value);
+            } while (!match && it.hasNext());
+
+            return value * fs.getFee() + this.minimum;
+        } else {
+            return this.minimum;
+        }
     }
-    
 }
