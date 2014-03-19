466bea472caa43ad8813d3534504cfacd6439418
Roobre <roobre@roobre.es>
2014-03-19 15:57:26 +0100

    Added javadoc to FeeStrategy interface.

--- src/es/unileon/ulebank/brokerage/FeeStrategy.java
+++ src/es/unileon/ulebank/brokerage/FeeStrategy.java
@@ -1,5 +1,12 @@
 package es.unileon.ulebank.brokerage;
 
 public interface FeeStrategy {
+
+    /**
+     * Returns the fee that should be applied to the given amount.
+     *
+     * @param value
+     * @return The fee that should be applied to the given amount
+     */
     public double getFee(double value);
 }
