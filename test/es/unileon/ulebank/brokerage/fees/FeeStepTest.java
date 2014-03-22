f6b2e830202649caf2aba17b5893b6eecdeea3a4
Jorge <dain735@dain735-Lenovo-G580.(none)>
2014-03-22 18:45:32 +0100

    Added test for FeeStep. Not completed.

--- test/es/unileon/ulebank/brokerage/fees/FeeStepTest.java
+++ test/es/unileon/ulebank/brokerage/fees/FeeStepTest.java
@@ -9,81 +9,89 @@ package es.unileon.ulebank.brokerage.fees;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+//import static org.hamcrest.CoreMatchers.*;
 
 /**
  *
  * @author roobre
  */
 public class FeeStepTest {
+	
+	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
+	FeeStep feeStep2 = new FeeStep(1000, 1200, 0.2);
+//	FeeStep feeStep3 = new FeeStep(1000, 500, 0.03);
+	FeeStep feeStep4 = new FeeStep(800, 1200, 0.5);
 
     public FeeStepTest() {
     }
 
     @Before
     public void setUp() {
+    	
     }
 
     @Test
     public void testGetFee() {
-        System.out.println("getFee");
-        FeeStep instance = null;
-        double expResult = 0.0;
-        double result = instance.getFee();
-        assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
+        assertEquals(0.01, feeStep1.getFee(), 0.0);
+        assertEquals(0.2, feeStep2.getFee(), 0.0);
+//        assertEquals(0.03, feeStep3.getFee(), 0.0);
+        assertEquals(0.5, feeStep4.getFee(), 0.0);
     }
 
     @Test
     public void testGetLow() {
-        System.out.println("getLow");
-        FeeStep instance = null;
-        double expResult = 0.0;
-        double result = instance.getLow();
-        assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
+        assertEquals(0, feeStep1.getLow(), 0.0);
+        assertEquals(1000, feeStep2.getLow(), 0.0);
+//        assertEquals(1000, feeStep3.getLow(), 0.0);
+        assertEquals(800, feeStep4.getLow(), 0.0);
     }
 
     @Test
     public void testGetHigh() {
-        System.out.println("getHigh");
-        FeeStep instance = null;
-        double expResult = 0.0;
-        double result = instance.getHigh();
-        assertEquals(expResult, result, 0.0);
-        fail("The test case is a prototype.");
+        assertEquals(1000, feeStep1.getHigh(), 0.0);
+        assertEquals(1200, feeStep2.getHigh(), 0.0);
+//        assertEquals(500, feeStep3.getHigh(), 0.0);
+        assertEquals(1200, feeStep4.getHigh(), 0.0);
     }
 
     @Test
     public void testWraps() {
-        System.out.println("wraps");
-        double value = 0.0;
-        FeeStep instance = null;
-        boolean expResult = false;
-        boolean result = instance.wraps(value);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        
+        assertTrue(feeStep1.wraps(100));
+        assertTrue(feeStep1.wraps(0));
+        assertFalse(feeStep1.wraps(1000));
+        assertFalse(feeStep1.wraps(1001));
+        
+        assertTrue(feeStep2.wraps(1100));
+        assertTrue(feeStep2.wraps(1000));
+        assertFalse(feeStep2.wraps(1200));
+        assertFalse(feeStep2.wraps(1234));
+        
+        assertTrue(feeStep4.wraps(800));
+        assertTrue(feeStep4.wraps(1000));
+        assertFalse(feeStep4.wraps(1200));
+        assertFalse(feeStep4.wraps(1234));
+        
     }
 
     @Test
     public void testCollides() {
-        System.out.println("collides");
-        FeeStep another = null;
-        FeeStep instance = null;
-        boolean expResult = false;
-        boolean result = instance.collides(another);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        assertFalse(feeStep1.collides(feeStep2));
+//        assertFalse(feeStep1.collides(feeStep3));
+        assertTrue(feeStep1.collides(feeStep1));
+        assertTrue(feeStep1.collides(feeStep4));
+        
+        assertTrue(feeStep2.collides(feeStep2));
+        assertTrue(feeStep2.collides(feeStep4));
+        
+        assertTrue(feeStep4.collides(feeStep4));
     }
 
     @Test
     public void testToString() {
-        System.out.println("toString");
-        FeeStep instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        assertEquals("[0.0,1000.0)", feeStep1.toString());
+        assertEquals("[1000.0,1200.0)", feeStep2.toString());
+        assertEquals("[800.0,1200.0)", feeStep4.toString());
     }
 
 }
