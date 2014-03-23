15ae96e8a35f9582c534a61a546a3124455eec08
Jorge <dain735@dain735-Lenovo-G580.(none)>
2014-03-23 14:14:36 +0100

    Test structure changed due to code changes

--- test/es/unileon/ulebank/brokerage/fees/ScaledPercentFeeTest.java
+++ test/es/unileon/ulebank/brokerage/fees/ScaledPercentFeeTest.java
@@ -16,11 +16,6 @@ import static org.junit.Assert.*;
  * @author roobre, dain735
  */
 public class ScaledPercentFeeTest {
-	
-	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
-	FeeStep feeStep2 = new FeeStep(1000, 1200, 0.2);
-	FeeStep feeStep3 = new FeeStep(1200, 2000, 0.03);
-	FeeStep feeStep4 = new FeeStep(800, 1200, 0.5);
 
     public ScaledPercentFeeTest() {
     }
@@ -31,6 +26,12 @@ public class ScaledPercentFeeTest {
 
     @Test
     public void testAddStep() throws Exception {
+    	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
+    	FeeStep feeStep2 = new FeeStep(1000, 1200, 0.2);
+    	FeeStep feeStep3 = new FeeStep(1200, 2000, 0.03);
+    	FeeStep feeStep4 = new FeeStep(800, 1200, 0.5);
+    	
+    	
         ScaledPercentFee instance = new ScaledPercentFee(0.0);
         instance.addStep(feeStep1);
         
@@ -40,7 +41,7 @@ public class ScaledPercentFeeTest {
 			e.printStackTrace();
 		}
         
-        instance.addStep(feeStep3);		//Throws CrossedStepException but it shouldn't
+        instance.addStep(feeStep3);
         
         try {
 			instance.addStep(feeStep4);
@@ -50,13 +51,17 @@ public class ScaledPercentFeeTest {
     }
 
     @Test
-    public void testGetFee() throws CrossedStepException {
+    public void testGetFee() throws CrossedStepException, InvalidFeeException, InvalidStepException {
+    	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
+    	FeeStep feeStep3 = new FeeStep(1200, 2000, 0.03);
+    	
     	ScaledPercentFee instance = new ScaledPercentFee(10);
     	instance.addStep(feeStep1);
-//    	instance.addStep(feeStep3);
+    	instance.addStep(feeStep3);
     	assertEquals(500*0.01+10, instance.getFee(500), 0.0);
     	assertEquals(0*0.01+10, instance.getFee(0), 0.0);
     	assertFalse(1100*0.01+10==(instance.getFee(1100)));		//1100 is not in feeStep1 but we are considering so
+    	assertEquals(1500*0.03+10, instance.getFee(1500), 0.0);
     	
     }
 
