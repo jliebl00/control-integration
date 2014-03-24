9a7dad85d079d89365d62624dd287f6ddc72a2b3
Jorge <dain735@dain735-Lenovo-G580.(none)>
2014-03-24 17:02:04 +0100

    Fixed structure of test code.

--- test/es/unileon/ulebank/brokerage/fees/ScaledPercentFeeTest.java
+++ test/es/unileon/ulebank/brokerage/fees/ScaledPercentFeeTest.java
@@ -17,51 +17,59 @@ import static org.junit.Assert.*;
  */
 public class ScaledPercentFeeTest {
 
+	FeeStep feeStep1;
+	FeeStep feeStep2;
+	FeeStep feeStep3;
+	FeeStep feeStep4;
+	ScaledPercentFee instance;
+	ScaledPercentFee instance2;
+	
     public ScaledPercentFeeTest() {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws InvalidStepException, InvalidFeeException {
+    	instance = new ScaledPercentFee(0.0);
+    	instance2 = new ScaledPercentFee(10.0);
+    	feeStep1 = new FeeStep(0, 1000, 0.01);
+    	feeStep2 = new FeeStep(900, 1200, 0.2);
+    	feeStep3 = new FeeStep(1200, 2000, 0.03);
+    	feeStep4 = new FeeStep(800, 1200, 0.5);
     }
 
     @Test
     public void testAddStep() throws Exception {
-    	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
-    	FeeStep feeStep2 = new FeeStep(1000, 1200, 0.2);
-    	FeeStep feeStep3 = new FeeStep(1200, 2000, 0.03);
-    	FeeStep feeStep4 = new FeeStep(800, 1200, 0.5);
-    	
-    	
-        ScaledPercentFee instance = new ScaledPercentFee(0.0);
         instance.addStep(feeStep1);
         
-        try {
-			instance.addStep(feeStep2);
-		} catch (CrossedStepException e) {
-			e.printStackTrace();
-		}
+//        try {
+//			instance.addStep(feeStep2);
+//		} catch (CrossedStepException e) {
+//			e.printStackTrace();
+//		}
         
         instance.addStep(feeStep3);
         
-        try {
-			instance.addStep(feeStep4);
-		} catch (CrossedStepException e) {
-			e.printStackTrace();
-		}
+//        try {
+//			instance.addStep(feeStep4);
+//		} catch (CrossedStepException e) {
+//			e.printStackTrace();
+//		}
+    }
+    
+    @Test(expected = CrossedStepException.class)
+    public void testCrossedStepException() throws InvalidStepException, InvalidFeeException, CrossedStepException {
+    	instance.addStep(feeStep1);
+    	instance.addStep(feeStep2);
     }
 
     @Test
-    public void testGetFee() throws CrossedStepException, InvalidFeeException, InvalidStepException {
-    	FeeStep feeStep1 = new FeeStep(0, 1000, 0.01);
-    	FeeStep feeStep3 = new FeeStep(1200, 2000, 0.03);
-    	
-    	ScaledPercentFee instance = new ScaledPercentFee(10);
-    	instance.addStep(feeStep1);
-    	instance.addStep(feeStep3);
-    	assertEquals(500*0.01+10, instance.getFee(500), 0.0);
-    	assertEquals(0*0.01+10, instance.getFee(0), 0.0);
-    	assertFalse(1100*0.01+10==(instance.getFee(1100)));		//1100 is not in feeStep1 but we are considering so
-    	assertEquals(1500*0.03+10, instance.getFee(1500), 0.0);
+    public void testGetFee() throws InvalidFeeException, CrossedStepException {
+    	instance2.addStep(feeStep1);
+    	instance2.addStep(feeStep3);
+    	assertEquals(500*0.01+10, instance2.getFee(500), 0.0);
+    	assertEquals(0*0.01+10, instance2.getFee(0), 0.0);
+    	assertFalse(1100*0.01+10==(instance2.getFee(1100)));		//1100 is not in feeStep1 but we are considering so
+    	assertEquals(1500*0.03+10, instance2.getFee(1500), 0.0);
     	
     }
 
