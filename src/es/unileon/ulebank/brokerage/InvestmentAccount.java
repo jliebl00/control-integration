45e363673149fba561446822d34436a3584be614
Roobre <roobre@roobre.es>
2014-03-25 17:07:41 +0100

    Now InvestmentAccount also holds the bought investment funds.

--- src/es/unileon/ulebank/brokerage/InvestmentAccount.java
+++ src/es/unileon/ulebank/brokerage/InvestmentAccount.java
@@ -4,22 +4,28 @@ import java.util.ArrayList;
 import es.unileon.ulebank.Account;
 
 public class InvestmentAccount extends Account {
-    
+
     private final ArrayList<StockPack> stockPacks;
-    
+    private final ArrayList<InvestmentFundPack> fundpacks;
+
     public InvestmentAccount() {
         this.stockPacks = new ArrayList<>();
+        this.fundpacks = new ArrayList<>();
     }
 
     public void addStockPack(StockPack stockPack) {
         this.stockPacks.add(stockPack);
     }
-    
+
+    public void addInvestmentFundPack(InvestmentFundPack pack) {
+        this.fundpacks.add(pack);
+    }
+
     /**
      * @return the stockPacks
      */
     public ArrayList<StockPack> getStockPacks() {
         return stockPacks;
     }
-    
+
 }
