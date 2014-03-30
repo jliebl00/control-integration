904688886f4e445004e2b6d1b31d36d398d0b4f3
Rut <Rut@Rut-PC>
2014-03-30 19:52:15 +0200

    Ballance office added

--- src/es/unileon/ulebank/Office.java
+++ src/es/unileon/ulebank/Office.java
@@ -7,156 +7,174 @@ import es.unileon.ulebank.handler.Handler;
 import es.unileon.ulebank.handler.IdOffice;
 
 /**
- * 
+ *
  * @author Patricia
- * 
+ *
  */
 public class Office {
 
-	/**
-	 * The id of the office
-	 */
-	private Handler idOffice;
-	/**
-	 * The costs of the local of the office
-	 */
-	private int localCost;
-	/**
-	 * The costs of the light, water and gas of the office
-	 */
-	private int utilitiesCost;
-	/**
-	 * The expenses in the salaries of the employees
-	 */
-	private int employeeCost;
-	/**
-	 * The total expenses or costs of the office
-	 */
-	private int totalExpenses;
-	/**
-	 * The total income of the office
-	 */
-	private int totalIncome;
-	/**
-	 * The list of employees of this office
-	 */
-	private ArrayList<Employee> employeeList;
-	/**
-	 * The list of accounts of this office
-	 */
-	private ArrayList<Account> accountList;
+    /**
+     * The id of the office
+     */
+    private Handler idOffice;
+    /**
+     * The costs of the local of the office
+     */
+    private int localCost;
+    /**
+     * The costs of the light, water and gas of the office
+     */
+    private int utilitiesCost;
+    /**
+     * The expenses in the salaries of the employees
+     */
+    private int employeeCost;
+    /**
+     * The total expenses or costs of the office
+     */
+    private int totalExpenses;
+    /**
+     * The total income of the office
+     */
+    private int totalIncome;
+    /**
+     * The total balance of the office
+     */
+    private int balance;
+    /**
+     * The list of employees of this office
+     */
+    private ArrayList<Employee> employeeList;
+    /**
+     * The list of accounts of this office
+     */
+    private ArrayList<Account> accountList;
 
-	/**
-	 * Constructor of the class
-	 * 
-	 * @throws MalformedHandlerException
-	 */
-	public Office(Handler idOffice) throws MalformedHandlerException {
-		this.idOffice = new IdOffice(idOffice.toString());
-	}
+    /**
+     * Constructor of the class
+     *
+     * @throws MalformedHandlerException
+     */
+    public Office(Handler idOffice) throws MalformedHandlerException {
+        this.idOffice = new IdOffice(idOffice.toString());
+    }
 
-	/**
-	 * Returns the id of the office
-	 */
-	public Handler getIdOffice() {
-		return idOffice;
-	}
+    /**
+     * Returns the id of the office
+     */
+    public Handler getIdOffice() {
+        return idOffice;
+    }
 
-	/**
-	 * Sets the id of the office
-	 */
-	public void setIdOffice(Handler idOffice) {
-		this.idOffice = idOffice;
-	}
+    /**
+     * Sets the id of the office
+     */
+    public void setIdOffice(Handler idOffice) {
+        this.idOffice = idOffice;
+    }
 
-	/**
-	 * Returns the expenses of the office
-	 */
-	public int getExpenses() {
-		return totalExpenses;
-	}
+    /**
+     * Returns the expenses of the office
+     */
+    public int getExpenses() {
+        return totalExpenses;
+    }
 
-	/**
-	 * Sets the total expenses of the office
-	 */
-	public void setExpenses(int localCost, int utilitiesCost, int employeeCost) {
+    /**
+     * Sets the total expenses of the office
+     */
+    public void setExpenses(int localCost, int utilitiesCost, int employeeCost) {
 
-		this.localCost = localCost;
-		this.utilitiesCost = utilitiesCost;
-		this.employeeCost = employeeCost;
+        this.localCost = localCost;
+        this.utilitiesCost = utilitiesCost;
+        this.employeeCost = employeeCost;
 
-		this.totalExpenses = this.localCost + this.utilitiesCost
-				+ this.employeeCost;
-	}
+        this.totalExpenses = this.localCost + this.utilitiesCost
+                + this.employeeCost;
+    }
 
-	/**
-	 * Returns the income of the office
-	 */
-	public int getTotalIncome() {
-		return totalIncome;
-	}
+    /**
+     * Returns the income of the office
+     */
+    public int getTotalIncome() {
+        return totalIncome;
+    }
 
-	/**
-	 * Sets the total income of the office
-	 */
-	public void setTotalIncome(int totalIncome) {
-		// Addition of the types of incomes.
-		this.totalIncome = totalIncome;
-	}
+    /**
+     * Sets the total income of the office
+     */
+    public void setTotalIncome(int totalIncome) {
+        // Addition of the types of incomes.
+        this.totalIncome = totalIncome;
+    }
 
-	/**
-	 * Returns a copy of the list of employees of the office
-	 */
-	public ArrayList<Employee> getEmployeeList() {
-		return new ArrayList<Employee>(employeeList);
-	}
+    /**
+     * Returns the balance of the office
+     */
+    public int getBalance() {
+        return balance;
+    }
 
-	/**
-	 * Sets the list of employees of the office
-	 */
-	public void setEmployeeList(ArrayList<Employee> employeeList) {
-		this.employeeList = employeeList;
-	}
+    /**
+     * Sets the total balance of the office
+     */
+    public void setBalance() {
+        this.balance = this.totalIncome - this.totalExpenses;
+    }
 
-	/**
-	 * Returns the list of accounts of the office
-	 */
-	public ArrayList<Account> getAccountList() {
-		return accountList;
-	}
+    /**
+     * Returns a copy of the list of employees of the office
+     */
+    public ArrayList<Employee> getEmployeeList() {
+        return new ArrayList<>(employeeList);
+    }
 
-	/**
-	 * Sets the list of accounts of the office
-	 */
-	public void setAccountList(ArrayList<Account> accountList) {
-		this.accountList = accountList;
-	}
+    /**
+     * Sets the list of employees of the office
+     */
+    public void setEmployeeList(ArrayList<Employee> employeeList) {
+        this.employeeList = employeeList;
+    }
 
-	/**
-	 * Adds an employee to the list of employees
-	 */
-	public void addEmployee(Employee employee) {
-		employeeList.add(employee);
-	}
+    /**
+     * Returns the list of accounts of the office
+     */
+    public ArrayList<Account> getAccountList() {
+        return accountList;
+    }
 
-	/**
-	 * Deletes an employee to the list of employees
-	 */
-	public void deleteEmployee(Employee employee) {
-		employeeList.remove(employee);
-	}
+    /**
+     * Sets the list of accounts of the office
+     */
+    public void setAccountList(ArrayList<Account> accountList) {
+        this.accountList = accountList;
+    }
 
-	/**
-	 * Adds an account to the list of accounts
-	 */
-	public void addAccount(Account account) {
-		accountList.add(account);
-	}
+    /**
+     * Adds an employee to the list of employees
+     */
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
 
-	/**
-	 * Deletes an account to the list of accounts
-	 */
-	public void deleteAccount(Account account) {
-		accountList.remove(account);
-	}
+    /**
+     * Deletes an employee to the list of employees
+     */
+    public void deleteEmployee(Employee employee) {
+        employeeList.remove(employee);
+    }
+
+    /**
+     * Adds an account to the list of accounts
+     */
+    public void addAccount(Account account) {
+        accountList.add(account);
+    }
+
+    /**
+     * Deletes an account to the list of accounts
+     */
+    public void deleteAccount(Account account) {
+        accountList.remove(account);
+    }
 }
