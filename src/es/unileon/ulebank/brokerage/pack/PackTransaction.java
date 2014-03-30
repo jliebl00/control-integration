f71357c10b756d1f4469620c034ac55cb313e698
Roobre <roobre@roobre.es>
2014-03-30 01:33:50 +0100

    Now PackTransaction also stores the operator (Employee) who ordered the transaction.

--- src/es/unileon/ulebank/brokerage/pack/PackTransaction.java
+++ src/es/unileon/ulebank/brokerage/pack/PackTransaction.java
@@ -5,6 +5,7 @@
  */
 package es.unileon.ulebank.brokerage.pack;
 
+import es.unileon.ulebank.Employee;
 import es.unileon.ulebank.history.Transaction;
 import es.unileon.ulebank.history.TransactionType;
 import java.util.Date;
@@ -17,16 +18,18 @@ import java.util.logging.Handler;
 public class PackTransaction extends Transaction {
 
     private final Pack pack;
+    private final Employee operator;
 
-    public PackTransaction(Handler id, double amount, Date date, String subject, Enum<TransactionType> type, Pack pack) {
+    public PackTransaction(Handler id, double amount, Date date, String subject, Enum<TransactionType> type, Pack pack, Employee operator) {
         super(id, amount, date, subject, type);
 
         this.pack = pack;
+        this.operator = operator;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", pack=" + this.pack;
+        return super.toString() + ", pack=" + this.pack + ", operator=" + this.operator;
     }
 
 }
