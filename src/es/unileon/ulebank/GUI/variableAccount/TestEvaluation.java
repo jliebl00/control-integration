a865ccc2a5babca3a5537d2983d942c1a3ac35ee
ultravioletgirl <aliciasp6@hotmail.com>
2014-03-31 22:33:59 +0200

    Class where the test will be evaluated (empty)

---
+++ src/es/unileon/ulebank/GUI/variableAccount/TestEvaluation.java
@@ -0,0 +1,12 @@
+package es.unileon.ulebank.GUI.variableAccount;
+
+public class TestEvaluation {
+	
+	String suitable, notSuitable;
+	
+	public String testResult(){
+		//TO-DO Once we had the to evaluate the client
+		return suitable;
+	}
+
+}
