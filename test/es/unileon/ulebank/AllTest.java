e5f907f17b2e5019c8ada46c7310266902626103
dorian <dorian@Deimos>
2014-04-29 20:27:04 +0200

    Test agrupados y comprobados. 96.59% superados

--- test/es/unileon/ulebank/AllTest.java
+++ test/es/unileon/ulebank/AllTest.java
@@ -10,7 +10,10 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     es.unileon.ulebank.PackageUlebankTest.class,
-    es.unileon.ulebank.handler.PackageHandlerTest.class
+    es.unileon.ulebank.handler.PackageHandlerTest.class,
+    es.unileon.ulebank.fees.PackageFeesTest.class,
+    es.unileon.ulebank.pack.PackagePackTest.class,
+    es.unileon.ulebank.brokerage.PackageBrokerageTest.class
 })
 
 public class AllTest {}
