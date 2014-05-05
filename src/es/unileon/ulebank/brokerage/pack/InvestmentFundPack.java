7baed4adc9fad7bfac8d6c83be5a3a1ad729c950
Roobre <roobre@roobre.es>
2014-05-05 10:37:19 +0200

    Small changes to Packs.

--- src/es/unileon/ulebank/brokerage/pack/InvestmentFundPack.java
+++ src/es/unileon/ulebank/brokerage/pack/InvestmentFundPack.java
@@ -1,11 +1,10 @@
 package es.unileon.ulebank.brokerage.pack;
 
-import es.unileon.ulebank.account.Account;
 import es.unileon.ulebank.brokerage.buyable.Buyable;
 
 public class InvestmentFundPack extends Pack {
 
-    public InvestmentFundPack(Buyable product, int amount, Account account) {
-        super(product, amount, account);
+    public InvestmentFundPack(Buyable product, int amount) {
+        super(product, amount);
     }
 }
--- src/es/unileon/ulebank/brokerage/pack/StockPack.java
+++ src/es/unileon/ulebank/brokerage/pack/StockPack.java
@@ -1,6 +1,5 @@
 package es.unileon.ulebank.brokerage.pack;
 
-import es.unileon.ulebank.account.Account;
 import es.unileon.ulebank.brokerage.buyable.Buyable;
 import es.unileon.ulebank.brokerage.buyable.NotEnoughParticipationsException;
 import java.util.Date;
@@ -10,8 +9,8 @@ public class StockPack extends Pack {
     private final double price;
     private final Date date;    
 
-    public StockPack(Buyable product, int amount, Account account, double price, Date date) {
-        super(product, amount, account);
+    public StockPack(Buyable product, int amount, double price, Date date) {
+        super(product, amount);
         
         this.price = price;
         this.date = date;
@@ -37,6 +36,6 @@ public class StockPack extends Pack {
         if(stockage < 0) {
             throw new NotEnoughParticipationsException();
         }
-        this.amount = stockage;
+        this.setAmount(stockage);
     }
 }
