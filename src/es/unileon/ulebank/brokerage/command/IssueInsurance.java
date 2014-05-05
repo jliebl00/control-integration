699df1c3d873badd87df336af11c653738ac493f
Roobre <roobre@roobre.es>
2014-05-05 11:04:13 +0200

    Added dummy commands.

---
+++ src/es/unileon/ulebank/brokerage/command/IssueInsurance.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package es.unileon.ulebank.brokerage.command;
+
+import es.unileon.ulebank.command.Command;
+import es.unileon.ulebank.exceptions.MalformedHandlerException;
+import es.unileon.ulebank.handler.Handler;
+import java.util.Date;
+
+/**
+ *
+ * @author roobre
+ */
+public class IssueInsurance implements Command {
+
+    @Override
+    public Date getEffectiveDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Handler getID() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void execute() throws MalformedHandlerException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void undo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void redo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
