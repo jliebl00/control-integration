d4e2ca6ed91d8f1772aa0960e31f14c27476ab4d
CarlitosMayo <CarlitosMayo@192.168.1.12>
2014-05-07 17:05:54 +0200

    Integrado la parte de assets, no esta terminada pero la dejamos aqui

--- src/es/unileon/assets/KindOfMethod.java
+++
@@ -1,9 +0,0 @@
-package es.unileon.assets;
-/**
- * Class enum for the types of loans methods
- * @author CarlitosMayo
- *
- */
-public enum KindOfMethod {
-	French,Italian,German
-}
--- src/es/unileon/assets/handler/Handler.java
+++
@@ -1,6 +0,0 @@
-package es.unileon.assets.handler;
-
-public interface Handler {
-	int compareTo(Handler another);
-	String toString();
-}
